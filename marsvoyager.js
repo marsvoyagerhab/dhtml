b2240b2182d6c86ac9afc994c3ae6512035e739f
Mars Voyager <77075769+marsvoyagerhab@users.noreply.github.com>
2021-01-24 14:18:33 +0100

    Add files via upload

--- marsvoyager.js
+++ marsvoyager.js
@@ -1,31 +1,85 @@
 "use strict"; // don't allow hoisting or undeclared variables
 
-function updatePage() {
-    let main = document.getElementById("Main"); // really used as footer
+const DEBUGMAIN = (true && (location.href.indexOf("file:") == 0));
 
-    if (main != null) {
+function updatePage() {
+    let footer = document.getElementById("Footer");
+    let depth = getAttribute(footer, "data-depth", "");
+    let topic = getAttribute(footer, "data-topic", null);
+    let version = getAttribute(footer, "data-version", null);
+
+    if (DEBUGMAIN) {
+        let head = document.getElementsByTagName("head")[0];
         
+        // check meta data
+        let metas = head.getElementsByTagName("meta");
+        for (let i = 0; i < metas.length; i++) {
+            let meta = metas[i];
+            if ((meta.name == "apple-mobile-web-app-title") && (meta.content.indexOf(topic) < 0)) {
+                alert ("<meta>" + meta.name + " = " + meta.content);
+            } else if ((meta.name == "application-name") && (meta.content.indexOf(topic) < 0)) {
+                alert ("<meta>" + meta.name + " = " + meta.content);
+            } else if ((meta.name == "msapplication-config") && (!meta.content.startsWith(depth))) {
+                alert ("<meta>" + meta.name + " = " + meta.content);
+            }
+        }
+        /*
+        // the hrefs have been resolved to full path
+        let links = document.getElementsByTagName("link");
+        for (let i = 0; i < links.length; i++) {
+            let link = links[i];
+            if ((!link.href.startsWith(depth)) && (!link.href.startsWith("http"))) {
+                alert ("<link>" + link.rel + " " + link.href);
+            }
+        }
+        */
+    }
+    
+    let main = document.getElementById("Main");
+    if (main != null) {
         let para = document.createElement("P");
         para.innerHTML = "";
         main.appendChild(para);
     }
     
-    
-    let footer = document.getElementById("Footer");
     if (footer != null) {
         let div = document.createElement("DIV");
         
-        let home = getAttribute(footer, "data-depth", "");
-        let topic = getAttribute(footer, "data-topic", null);
-        let version = getAttribute(footer, "data-version", null);
+        let title = "Mars Voyager on Creativity and DHTML";
         
-        let html = "<hr/><a href=\"" + home + "index.html\">Mars Voyager on Creativity and DHTML</a>";
-        html = html + ((topic == null) ? "" : " : " + topic);
-        html = html + ((version == null) ? "" : ", version " + version);
-        html = html + "<br/>";
-        html = html + "<a href='https://github.com/marsvoyagerhab/dhtml/issues'>Submit an issue via GitHub</a>";
+        let txt = "";
+        div.appendChild(document.createElement("HR"));
         
-        div.innerHTML = html;
+        let span = document.createElement("SPAN");
+        if ((depth != null) && (depth != "")) {
+            let a = document.createElement("A");
+            a.href = depth + "index.html";
+            a.innerHTML = title;
+            span.appendChild(a);
+        } else {
+            txt = txt + title;
+        }
+        if (((topic != null) && (topic != "")) || ((version != null) && (version != ""))) {
+            txt = txt + " : ";
+        }
+        if ((topic != null) && (topic != "")) {
+            txt = txt + topic;
+        }
+        if ((version != null) && (version != "")) {
+            if ((topic != null) && (topic != "")) {
+                txt = txt + ", ";
+            }
+            txt = txt + "version " + version;
+        }
+        span.appendChild(document.createTextNode(txt));
+        div.appendChild(span);
+        
+        div.appendChild(document.createElement("BR"));
+        
+        let a = document.createElement("A");
+        a.href = "https://github.com/marsvoyagerhab/dhtml/issues";
+        a.innerHTML = "Submit an issue via GitHub";
+        div.appendChild(a);
         
         footer.appendChild(div);
     }
